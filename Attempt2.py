22ce4a6f6b5bfde81a346644ebc95a08458a32ab
SAulerich <166335486+SAulerich@users.noreply.github.com>
2024-09-15 19:31:56 -0400

    Update Attempt2.py

--- Attempt2.py
+++ Attempt2.py
@@ -5,5 +5,5 @@ kilometers = float(input("Enter distance in kilomteres: "))
 miles = kilometers * 0.6213
 
 # Output the results
-print(f"\n{kilometers} km = {miles} mi\n\n")
+print(f"\n{kilometers} km = {miles} mi\n\n") # Adding the new line code to create an easier to read aesthetic for the user
 print(f"Press any button to exit") # Adding this line of code because the results don't populate before the entire code closes out on itself
