2aa3992e2d3d9c409a79826cf0ee70e46fb2d608
SAulerich <166335486+SAulerich@users.noreply.github.com>
2024-09-15 19:29:29 -0400

    Update Attempt2.py

--- Attempt2.py
+++ Attempt2.py
@@ -6,4 +6,4 @@ miles = kilometers * 0.6213
 
 # Output the results
 print(f"\n{kilometers} km = {miles} mi\n\n")
-print(f"Press any button to exit")
+print(f"Press any button to exit") # Adding this line of code because the results don't populate before the entire code closes out on itself
