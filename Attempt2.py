3c3b5ecf18afa58088b54afd601dddb8b07883f1
SAulerich <166335486+SAulerich@users.noreply.github.com>
2024-09-15 19:21:43 -0400

    Add files via upload

---
+++ Attempt2.py
@@ -0,0 +1,9 @@
+# Writing a script that converts Kilometers to Miles
+kilometers = float(input("Enter distance in kilomteres: "))
+
+# Converting to miles (1 Km = 0.6213)
+miles = kilometers * 0.6213
+
+# Output the results
+print(f"\n{kilometers} km = {miles} mi\n\n")
+print(f"Press any button to exit")
---
+++ Attempt5.py
@@ -0,0 +1,10 @@
+# Writing a script that converts Kilometers to Miles
+kilometers = float(input("Enter distance in kilomteres: "))
+
+# Converting to miles (1 Km = 0.6213)
+miles = kilometers * 0.6213
+
+# Output the results
+print(f"\n{kilometers} km = {miles}:2f mi\n\n")
+print(f"Press the 'ENTER' button to exit")
+input()
