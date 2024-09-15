2996b37b031f67c47154ecf84e529e9716efdaa7
SAulerich <166335486+SAulerich@users.noreply.github.com>
2024-09-15 19:28:34 -0400

    Update Attempt1.py

--- Attempt1.py
+++ Attempt1.py
@@ -5,4 +5,4 @@ kilometers = float(input("Enter distance in kilomteres: "))
 miles = kilometers * 0.6213
 
 # Output the results
-print(f"[kilometers] km = [miles] mi")
+print(f"[kilometers] km = [miles] mi") # Used the wrong brackets which does not pull the information from the previous lines
